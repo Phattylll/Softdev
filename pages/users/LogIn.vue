3ddfc2bb65eb32a85f6e8671c497a5527bce86c6
Phattharanit <69140719+Phattylll@users.noreply.github.com>
2022-04-23 16:23:08 +0700

    Add files via upload

--- pages/users/LogIn.vue
+++ pages/users/LogIn.vue
@@ -1,5 +1,6 @@
 <template lang="html">
 <div class="center">
+  <button type="button" class="cancelbtn"><h3 style="color:white">Login</h3></button>
     <v-text-field v-model="email" label="Email"></v-text-field>
     <v-text-field
       v-model="password"
@@ -12,11 +13,14 @@
 
 </style>
 
-<button type="button" class="cancelbtn">Cancel</button>
-
-
+<span class="psw">Forgot <a href="/users/Forgot">password</a></span>
+<span class="Text1">Frist time at KMITL square </span>
+<span class="Regis"> <a href="/users/Regis">Resgister here</a></span>
+<div class="box Hog"><h1 style="color:white">Login</h1></div>
 
   </div>
+
+
 
 </template>
 
@@ -58,17 +62,55 @@ export default {
   width: 60%;
   border: 4px solid rgb(243, 95, 16);
   padding: 30px;
-  padding-top: 10px;
-  padding-bottom: 150px;
+  padding-top: 100px;
+  padding-bottom: 200px;
 
 }
 
   .cancelbtn {
-      float: right;
-      margin: 90px 0 12px 0;
-      width: auto;
+
+      margin: 0;
+      position: absolute;
+      top: 600px;
+      left: 50%;
+      width: 50%;
       padding: 7px 15px;
-      background-color: #f44336;
+      -ms-transform: translate(-50%, -50%);
+      transform: translate(-50%, -50%);
+      background-color: rgb(243, 95, 16); 
+    }
+  span.psw {
+      position: absolute;
+      top: 410px;
+      right: 22%;
+      }
+
+   span.Text1 {
+      position: absolute;
+      top: 500px;
+      right: 22%
+      }    
+
+
+ span.Regis {
+      position: absolute;
+      top: 520px;
+      right: 22%
+      } 
+  .box {
+    text-align: center;
+    position: absolute;
+     margin: -254px -34px;
+     width: 60%;
+     border: 4px solid rgb(243, 95, 16);
+     padding: 30px;
+     padding-top: 30px;
+     padding-bottom: 30px;
+
+    }
+    .Hog {
+        background-color: rgb(243, 95, 16);
+        position: absolute;
     }
 
 </style>
